2c526eea04a763769c81ea595c86a366dca251b8
kello22e <kello22e@mtholyoke.edu>
2018-07-24 18:34:32 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,6 +1,6 @@
 #puts "H"
 
-def holiday_greeting(to="Mom",from="From Your Favorite Child",holiday="Mother's Day")
+def holiday_greeting(to="Mom",from="From Your Favorite Child", holiday="Mother's Day")
   puts "Happy #{holiday}, #{to}! #{from}"
 end 
 
